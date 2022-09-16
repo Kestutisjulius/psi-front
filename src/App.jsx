8036ebdff11b2b58bc01741e44591aad3b739e48
Rugile <rugilestasionyte@gmail.com>
2022-09-16 09:17:43 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [labas, setLabas] = useState('');
+
+  useEffect(()=>{
+    axios.get('http://psi.back/second')
+    .then(res => {
+      setLabas(res.data.message);
+    })
+  }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {labas}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="http://psi.back/second"
           target="_blank"
           rel="noopener noreferrer"
         >
