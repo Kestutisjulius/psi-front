e70d1a7bdc4edd95fe17e8b76c171291753ce569
Kristina <crislayn@yahoo.com>
2022-09-19 15:30:09 +0300

    nav & main imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
 
 function App() {
   const [labas, setLabas] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get('http://psi.back/second')
-    .then(res => {
-      setLabas(res.data.message);
-    })
+      .then(res => {
+        setLabas(res.data.message);
+      })
   }, []);
-  
+
   return (
     <div className="App">
-      
-        <Nav />
+
+      <Nav />
       <Main />
-        <p>
-          {labas}
-        </p>
-      
-      
+      <p>
+        {labas}
+      </p>
+
+
 
     </div>
   );
